ba45b4b1c02abea48666973606a6c24b415cc688
osigram <igor.b769@gmail.com>
2023-07-14 19:24:08 +0300

    Solution

--- main.go
+++ main.go
@@ -1,1 +1,52 @@
-package BAD2023_test
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic("File opening error")
+	}
+	textBytes, err := io.ReadAll(file)
+	if err != nil {
+		panic("File reading error")
+	}
+	text := string(textBytes)
+
+	re := regexp.MustCompile("\\w+")
+	words := re.FindAllString(text, -1)
+	found := make([]string, 0, len(words))
+
+	for _, word := range words {
+		wordSlice := strings.Split(word, "")
+
+		found = append(found, getSymbol(wordSlice))
+		continue
+	}
+
+	fmt.Println(getSymbol(found))
+}
+
+func getSymbol(wordSlice []string) string {
+	var result string
+
+symbolOut:
+	for num, symbol := range wordSlice {
+		for _, symbol2 := range wordSlice[num+1:] {
+			if symbol == symbol2 {
+				continue symbolOut
+			}
+		}
+
+		result = symbol
+		break
+	}
+
+	return result
+}
